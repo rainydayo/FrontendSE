bc3b8f99e606d85f9540c7e65f3149aa9aedea45
Chayaphon Kingkaewkanthong <6633046121@student.chula.ac.th>
2024-05-01 05:19:21 +0700

    change cypress test to use deployment link instead local

--- cypress/e2e/epic1.cy.ts
+++ cypress/e2e/epic1.cy.ts
@@ -1,18 +1,18 @@
 describe('epic 1 user story 1-1', () => {
     beforeEach(()=>{
-      //cy.visit('https://restaurantca.vercel.app')
-      cy.visit('http://localhost:3000')
-      cy.wait(6000)
+      cy.visit('https://restaurantca.vercel.app')
+      //cy.visit('http://localhost:3000')
+      //cy.wait(2000)
       cy.get('[data-test="login-btn"]').click()
       cy.get('#input-email-for-credentials-provider').type("bunmee@gmail.com")
       cy.get('#input-password-for-credentials-provider').type("admin123")
       cy.get('.provider').find('button').click()
-      cy.wait(6000)
+      //cy.wait(2000)
     })
   
     it('TC-1 search with existing retaurant name & correct price range', () => {
       cy.get('.topmenu_itemcontainer__xd8j8').contains('Restaurant').click()
-      cy.wait(4000)
+      //cy.wait(2000)
       cy.get('[data-test="search-bar"]').type('Terry Crooks')
       cy.get('select[id="min"]').select(2) //value in index 2 option is 3
       cy.get('select[id="max"]').select(1) //value in index 1 option is 4
@@ -20,7 +20,7 @@ describe('epic 1 user story 1-1', () => {
       // cy.intercept("GET", `${process.env.BACKEND_URL}/api/v1/restaurants/search/:search?restaurantName=Terry Crooks&minPrice=3&maxPrice=4`, {
       //   fixture: 'tc1search.json'
       // })
-      cy.wait(2000)
+      //cy.wait(2000)
       cy.get('[data-test="restaurant-card-name"]').contains('Terry Crooks')
       cy.get('[data-test="catalog-div"]').children().should('have.length', 1).each(($child) => {
         cy.get('[data-test="price-range"]').should('contain', '$$$')
@@ -30,14 +30,14 @@ describe('epic 1 user story 1-1', () => {
   
     it('TC-2 search with empty text', () => {
       cy.get('.topmenu_itemcontainer__xd8j8').contains('Restaurant').click()
-      cy.wait(4000)
+      //cy.wait(2000)
       cy.get('select[id="min"]').select(2) //value in index 2 option is 3
       cy.get('select[id="max"]').select(2) //value in index 2 option is 3
       cy.get('[data-test="search-submit"]').click()
       // cy.intercept("GET", `${process.env.BACKEND_URL}/api/v1/restaurants/search/:search?restaurantName=&minPrice=3&maxPrice=3`, {
       //   fixture: 'tc2search.json'
       // })
-      cy.wait(2000)
+      //cy.wait(2000)
       cy.get('[data-test="catalog-div"]').children().should('have.length', 3).each(($child) => {
         cy.wrap($child).should('contain', '$$$')
         cy.wrap($child).should('not.contain', '$$$$')
@@ -46,14 +46,14 @@ describe('epic 1 user story 1-1', () => {
   
     it('TC-3 search with partial string of existing retaurant name & correct price range', () => {
       cy.get('.topmenu_itemcontainer__xd8j8').contains('Restaurant').click()
-      cy.wait(4000)
+      //cy.wait(2000)
       cy.get('[data-test="search-bar"]').type('Teenoi')
       cy.get('select[id="min"]').select(4) //value in index 4 option is 5
       cy.get('[data-test="search-submit"]').click()
       // cy.intercept("GET", `${process.env.BACKEND_URL}/api/v1/restaurants/search/:search?restaurantName=Teenoi&minPrice=5&maxPrice=5`, {
       //   fixture: 'tc3search.json'
       // })
-      cy.wait(2000)
+      //cy.wait(2000)
       cy.get('[data-test="catalog-div"]').children().should('have.length', 1).each(($child) => {
         cy.get('[data-test="price-range"]').should('have.text', '$$$$$')
       })
@@ -61,33 +61,33 @@ describe('epic 1 user story 1-1', () => {
     
     it('TC-4 search with partial string of existing retaurant name but incorrect price range', () => {
       cy.get('.topmenu_itemcontainer__xd8j8').contains('Restaurant').click()
-      cy.wait(4000)
+      //cy.wait(2000)
       cy.get('[data-test="search-bar"]').type('Teenoi')
       cy.get('select[id="max"]').select(3) //value in index 3 option is 2
       cy.get('[data-test="search-submit"]').click()
-      cy.wait(2000)
+      //cy.wait(2000)
       cy.get('[data-test="catalog-div"]').children().should('have.length', 0)
     })
     
     it('TC-5 search with non-existing restaurant name', () => {
       cy.get('.topmenu_itemcontainer__xd8j8').contains('Restaurant').click()
-      cy.wait(4000)
+      //cy.wait(2000)
       cy.get('[data-test="search-bar"]').type('aefkgnaeign')
       cy.get('[data-test="search-submit"]').click()
-      cy.wait(2000)
+      //cy.wait(2000)
       cy.get('[data-test="catalog-div"]').children().should('have.length', 0)
     })
   
     it('TC-6 search with empty text', () => {
       cy.get('.topmenu_itemcontainer__xd8j8').contains('Restaurant').click()
-      cy.wait(4000)
+      //cy.wait(2000)
       cy.get('select[id="min"]').select(1) //value in index 1 option is 2
       cy.get('select[id="max"]').select(1) //value in index 1 option is 4
       cy.get('[data-test="search-submit"]').click()
       // cy.intercept("GET", `${process.env.BACKEND_URL}/api/v1/restaurants/search/:search?restaurantName=&minPrice=2&maxPrice=4`, {
       //   fixture: 'tc6search.json'
       // }) 
-      cy.wait(2000)
+      //cy.wait(2000)
       cy.get('[data-test="catalog-div"]').children().should('have.length', 10)
       .each(($child) => {
         cy.get('[data-test="price-range"]').should('contain', '$$')
@@ -97,11 +97,11 @@ describe('epic 1 user story 1-1', () => {
   
     it('TC-7 search with incorrect price range', () => {
       cy.get('.topmenu_itemcontainer__xd8j8').contains('Restaurant').click()
-      cy.wait(4000)
+      //cy.wait(2000)
       cy.get('select[id="min"]').select(3) //value in index 3 option is 4
       cy.get('select[id="max"]').select(2) //value in index 2 option is 3
       cy.get('[data-test="search-submit"]').click()
-      cy.wait(2000)
+      //cy.wait(2000)
       cy.get('[data-test="catalog-div"]').children().should('have.length', 0)
     })
    
@@ -116,49 +116,49 @@ describe('epic 1 user story 1-1', () => {
   
   describe('epic 1 user story 1-2', () => {
     beforeEach(()=>{
-      //cy.visit('https://restaurantca.vercel.app')
-      cy.visit('http://localhost:3000')
-      cy.wait(6000)
+      cy.visit('https://restaurantca.vercel.app')
+      //cy.visit('http://localhost:3000')
+      //cy.wait(2000)
       cy.get('[data-test="login-btn"]').click()
       cy.get('#input-email-for-credentials-provider').type("bunmee@gmail.com")
       cy.get('#input-password-for-credentials-provider').type("admin123")
       cy.get('.provider').find('button').click()
-      cy.wait(6000)
+      //cy.wait(2000)
     })
   
     it('TC-8 show review if there is existing review', () => {
         cy.get('.topmenu_itemcontainer__xd8j8').contains('Restaurant').click()
-        cy.wait(4000)
+        //cy.wait(2000)
         cy.get('[data-test="restaurant-link-Kot Shop"]').click()
         cy.get('[data-test="review-card"]').should('exist')
     })
 
     it('TC-9 show no review if there is no exist review', () => {
         cy.get('.topmenu_itemcontainer__xd8j8').contains('Restaurant').click()
-        cy.wait(4000)
+        //cy.wait(2000)
         cy.get('[data-test="restaurant-link-The Cozy Cauldron"]').click()
         cy.get('[data-test="review-card"]').should('not.exist')
     })
 
     it('TC-10 comment with non-empty comment', () => {
         cy.get('.topmenu_itemcontainer__xd8j8').contains('Restaurant').click()
-        cy.wait(4000)
+        //cy.wait(2000)
         cy.get('[data-test="restaurant-link-Kot Shop"]').click()
         cy.get('[data-test="comment-text"]').type('Good job')
         cy.get('select[id="rating"]').select(2) //value in index 2 option is 3
         cy.get('[data-test="submit-review"]').click()
-        cy.wait(2000)
+        //cy.wait(2000)
         cy.get('[data-test="rating-3"]').should('exist')
         cy.get('[data-test="comment-Good job"]').should('exist') 
     })
 
     it('TC-11 comment with empty comment', () => {
         cy.get('.topmenu_itemcontainer__xd8j8').contains('Restaurant').click()
-        cy.wait(4000)
+        //cy.wait(2000)
         cy.get('[data-test="restaurant-link-Kot Shop"]').click()
         cy.get('select[id="rating"]').select(3) //value in index 3 option is 4
         cy.get('[data-test="submit-review"]').click()
-        cy.wait(2000)
+        //cy.wait(2000)
         cy.get('[data-test="rating-4"]').should('exist')
         cy.get('[data-test="comment- "]').should('exist') 
     })
@@ -166,19 +166,19 @@ describe('epic 1 user story 1-1', () => {
 
   describe('epic 1 user story 1-3', () => {
     beforeEach(()=>{
-      //cy.visit('https://restaurantca.vercel.app')
-      cy.visit('http://localhost:3000')
-      cy.wait(6000)
+      cy.visit('https://restaurantca.vercel.app')
+      //cy.visit('http://localhost:3000')
+      //cy.wait(2000)
       cy.get('[data-test="login-btn"]').click()
       cy.get('#input-email-for-credentials-provider').type("bunmee@gmail.com")
       cy.get('#input-password-for-credentials-provider').type("admin123")
       cy.get('.provider').find('button').click()
-      cy.wait(6000)
+      //cy.wait(2000)
     })
   
     it('TC-12 Every restaurant on the listing page displays a price range indicator.', () => {
         cy.get('.topmenu_itemcontainer__xd8j8').contains('Restaurant').click()
-        cy.wait(4000)
+        //cy.wait(2000)
         cy.get('[data-test="catalog-div"]').children().each(($child) => {
             cy.wrap($child).should('contain', '$')
           })
@@ -186,22 +186,22 @@ describe('epic 1 user story 1-1', () => {
 
     it('TC-13 The specific restaurant page does not display a price range indicator.', () => {
         cy.get('.topmenu_itemcontainer__xd8j8').contains('Restaurant').click()
-        cy.wait(4000)
+        //cy.wait(2000)
         cy.get('[data-test="restaurant-link-The Cozy Cauldron"]').click()
-        cy.wait(3000)
+        //cy.wait(2000)
         cy.get('div').contains('$').should('not.exist')
     })
 
     it('TC-14 display price range indicator correctly', () => {
         cy.get('.topmenu_itemcontainer__xd8j8').contains('Restaurant').click()
-        cy.wait(4000)
+        //cy.wait(2000)
         cy.get('select[id="min"]').select(1) //value in index 1 option is 2
         cy.get('select[id="max"]').select(3) //value in index 3 option is 2
         cy.get('[data-test="search-submit"]').click()
         // cy.intercept("GET", `${process.env.BACKEND_URL}/api/v1/restaurants/search/:search?restaurantName=&minPrice=3&maxPrice=3`, {
         //   fixture: 'tc2search.json'
         // })
-        cy.wait(2000)
+        //cy.wait(2000)
         cy.get('[data-test="catalog-div"]').children().each(($child) => {
           cy.get('[data-test="price-range"]').should('not.have.text', '$')
           cy.get('[data-test="price-range"]').should('contain', '$$')
@@ -215,26 +215,26 @@ describe('epic 1 user story 1-1', () => {
   
   describe('epic 1 user story 1-4', () => {
     beforeEach(()=>{
-      //cy.visit('https://restaurantca.vercel.app')
-      cy.visit('http://localhost:3000')
-      cy.wait(6000)
+      cy.visit('https://restaurantca.vercel.app')
+      //cy.visit('http://localhost:3000')
+      //cy.wait(2000)
       cy.get('[data-test="login-btn"]').click()
       cy.get('#input-email-for-credentials-provider').type("bunmee@gmail.com")
       cy.get('#input-password-for-credentials-provider').type("admin123")
       cy.get('.provider').find('button').click()
-      cy.wait(6000)
+      //cy.wait(2000)
     })
 
     it('TC-15 All promotions page display all restaurant promotion', () => {
         cy.get('.topmenu_itemcontainer__xd8j8').contains('Promotion').click()
-        cy.wait(4000)
+        //cy.wait(2000)
         cy.get('[data-test="promotion"]').should('exist')
         cy.get('[data-test="promotion"]').should('have.length', 15)
     })
 
     it('TC-16 Restaurant which has promotion display promotion correctly', () => {
         cy.get('.topmenu_itemcontainer__xd8j8').contains('Restaurant').click()
-        cy.wait(4000)
+        //cy.wait(2000)
         cy.get('[data-test="restaurant-link-Amezon"]').click()
         cy.get('[data-test="promotion"]').should('exist')
         cy.get('[data-test="promotion"]').should('have.length', 4)
@@ -243,7 +243,7 @@ describe('epic 1 user story 1-1', () => {
 
     it('TC-17 Restaurant which has no promotion display "No Promotion Available"', () => {
         cy.get('.topmenu_itemcontainer__xd8j8').contains('Restaurant').click()
-        cy.wait(4000)
+        //cy.wait(2000)
         cy.get('[data-test="restaurant-link-Kot Shop"]').click()
         cy.get('[data-test="promotion"]').should('not.exist')
         cy.get('[data-test="no-promotion-text"]').should('exist')
